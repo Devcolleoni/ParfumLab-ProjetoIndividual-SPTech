0f226ebe7491bf543cb96fcb6e5098da5281c1b4
Devcolleoni <gustavo.carvalho@sptech.school>
2026-05-27 20:38:29 -0300

    Funcionando com a Vm

--- web-data-viz/src/models/dashboardModel.js
+++ web-data-viz/src/models/dashboardModel.js
@@ -3,86 +3,75 @@ var database = require("../database/config");
 function buscarNotas() {
 
     var instrucaoSql = `
-
         SELECT
-            perfume.nome,
-            ROUND(AVG(avaliacao.qtdEstrela), 1) AS media
-        FROM avaliacao
-        JOIN perfume
-            ON avaliacao.fkPerfume = perfume.id
-        GROUP BY perfume.nome;
-
+            p.nome,
+            ROUND(AVG(a.qtdEstrela), 1) AS media
+        FROM Avaliacao a
+        JOIN Perfume p
+            ON a.fkPerfume = p.id
+        GROUP BY p.nome;
     `;
 
     console.log("Executando SQL: \n" + instrucaoSql);
 
     return database.executar(instrucaoSql);
-
 }
 
 function buscarQuantidade() {
 
     var instrucaoSql = `
-
         SELECT
-            perfume.nome,
-            COUNT(avaliacao.id) AS quantidade
-        FROM avaliacao
-        JOIN perfume
-            ON avaliacao.fkPerfume = perfume.id
-        GROUP BY perfume.nome;
-
+            p.nome,
+            COUNT(a.id) AS quantidade
+        FROM Avaliacao a
+        JOIN Perfume p
+            ON a.fkPerfume = p.id
+        GROUP BY p.nome;
     `;
 
     console.log("Executando SQL: \n" + instrucaoSql);
 
     return database.executar(instrucaoSql);
-
 }
 
 function buscarMetricas() {
 
     var instrucaoSql = `
-
         SELECT
 
         (
-            SELECT perfume.nome
-            FROM avaliacao
-            JOIN perfume
-                ON avaliacao.fkPerfume = perfume.id
-            GROUP BY perfume.nome
-            ORDER BY COUNT(avaliacao.id) DESC
+            SELECT p.nome
+            FROM Avaliacao a
+            JOIN Perfume p
+                ON a.fkPerfume = p.id
+            GROUP BY p.nome
+            ORDER BY COUNT(a.id) DESC
             LIMIT 1
         ) AS maisAvaliado,
 
         (
-            SELECT perfume.nome
-            FROM avaliacao
-            JOIN perfume
-                ON avaliacao.fkPerfume = perfume.id
-            GROUP BY perfume.nome
-            ORDER BY COUNT(avaliacao.id) ASC
+            SELECT p.nome
+            FROM Avaliacao a
+            JOIN Perfume p
+                ON a.fkPerfume = p.id
+            GROUP BY p.nome
+            ORDER BY COUNT(a.id) ASC
             LIMIT 1
         ) AS menosAvaliado,
 
         (
             SELECT COUNT(*)
-            FROM avaliacao
+            FROM Avaliacao
         ) AS totalAvaliacoes;
-
     `;
 
     console.log("Executando SQL: \n" + instrucaoSql);
 
     return database.executar(instrucaoSql);
-
 }
 
 module.exports = {
-
     buscarNotas,
     buscarQuantidade,
     buscarMetricas
-
 };
